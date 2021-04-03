a608fb5b08aad754ab8104474fb037c3fbc01d8a
Bhavesh Joshi <34483890+gokulbhaveshjoshi@users.noreply.github.com>
2021-04-03 20:20:48 +0530

    Final code

---
+++ app/src/main/java/com/example/mydemo/MainActivity2.java
@@ -0,0 +1,48 @@
+package com.example.mydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity2 extends AppCompatActivity {
+    ImageView img;
+    private ScaleGestureDetector scaleGestureDetector;
+    private float mScaleFactor = 1.0f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+         Intent intent = getIntent();
+         String uid = intent.getStringExtra("imageurl");
+
+
+        img=(ImageView)findViewById(R.id.image);
+//        img.setImageResource(getIntent().getIntExtra("imageUrl", R.drawable.ic_launcher_background));
+        Glide.with(getApplicationContext()).load(uid).into(img);
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        scaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+            img.setScaleX(mScaleFactor);
+            img.setScaleY(mScaleFactor);
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mydemo/MyAdapter.java
@@ -0,0 +1,67 @@
+package com.example.mydemo;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    private ArrayList<Model> mList;
+    private Context context;
+
+    public MyAdapter(Context context , ArrayList<Model> mList){
+
+        this.context = context;
+        this.mList = mList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item, parent ,false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        final Model temp=mList.get(position);
+        Glide.with(context).load(mList.get(position).getImageUrl()).into(holder.imageView);
+        //holder.img.setImageResource(Integer.parseInt(mList.get(position).getImageUrl()));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(context, MainActivity2.class);
+
+                intent.putExtra("imageurl",temp.getImageUrl());
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+
+}
